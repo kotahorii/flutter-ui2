1f169639bdcb4af715bfde0270e64ceb029f48b7
kotahorii <khorii.micron@gmail.com>
2022-01-31 14:31:29 +0900

    [Update] header rayout

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,172 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        children: [
-
-        ],
+        children: [],
       ),
     );
   }
 }
 
+class _HeaderCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    return Path()
+      ..lineTo(0, size.height * 0.5)
+      ..quadraticBezierTo(
+          size.width * 0.55, size.height, size.width, size.height * 0.6)
+      ..lineTo(size.width, 0)
+      ..close();
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class _HeaderBackground extends StatelessWidget {
+  final double height;
+
+  const _HeaderBackground({Key? key, required this.height}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _HeaderCurveClipper(),
+      child: Container(
+        width: double.infinity,
+        height: height,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+                colors: [Color(0xFFFD9766), Color(0xFFFF7362)],
+                stops: [0, 1])),
+      ),
+    );
+  }
+}
+
+class _HeaderCirclePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.white.withOpacity(0.4)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 6;
+
+    canvas.drawCircle(
+      Offset(size.width * 0.25, size.height * 0.4),
+      12,
+      paint,
+    );
+    canvas.drawCircle(Offset(size.width * 0.75, size.height * 0.2), 12, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class _HeaderCircle extends StatelessWidget {
+  final double height;
+
+  const _HeaderCircle({Key? key, required this.height}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _HeaderCirclePainter(),
+      child: Container(
+        width: double.infinity,
+        height: height,
+      ),
+    );
+  }
+}
+
+class _HeaderTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'Welcome',
+          style: Theme.of(context)
+              .textTheme
+              .headline4!
+              .copyWith(color: kTextColorPrimary, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          'Sign in to continue',
+          style: Theme.of(context)
+              .textTheme
+              .subtitle2!
+              .copyWith(color: kTextColorPrimary),
+        )
+      ],
+    );
+  }
+}
+
+class _HeaderBackButton extends StatelessWidget {
+  const _HeaderBackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        style: TextButton.styleFrom(
+            primary: kButtonTextColorPrimary,
+            backgroundColor: Colors.transparent,
+            shape: const CircleBorder(
+                side: BorderSide(color: kButtonColorPrimary))),
+        onPressed: () {},
+        child: const Icon(
+          Icons.chevron_left_outlined,
+          color: kIconColor,
+        ));
+  }
+}
+
+class _Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const double height = 320;
+    return Container(
+      height: height,
+      child: Stack(
+        children: [
+          const Align(
+            alignment: Alignment.topCenter,
+            child: _HeaderBackground(
+              height: height,
+            ),
+          ),
+          const Align(
+            alignment: Alignment.topCenter,
+            child: _HeaderCircle(
+              height: height,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 128),
+              child: _HeaderTitle(),
+            ),
+          ),
+          const Positioned(
+            child: _HeaderBackButton(),
+            top: 16,
+            left: 0,
+          )
+        ],
+      ),
+    );
+  }
+}
